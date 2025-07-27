c31fda69c125880153b28642340dc3215355fb27
Shailaja Poojari <shailaja.s.poojary091@gmail.com>
2025-07-27 22:41:43 +0530

    Day 4: Added page.php, front-page.php and configured static homepage

---
+++ Week1/Day4/front-page.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main>
+    <h2>Welcome to My Custom Homepage</h2>
+    <p>This is a custom front page designed as part of my WordPress internship at QTechSolutions.</p>
+</main>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
