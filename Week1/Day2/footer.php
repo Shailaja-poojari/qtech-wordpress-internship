b49c5f2e86a7f47d6483dda02bd825575eef182d
Shailaja Poojari <shailaja.s.poojary091@gmail.com>
2025-07-27 19:20:17 +0530

    Day 2 - Added header.php and footer.php, updated index.php

---
+++ Week1/Day2/footer.php
@@ -0,0 +1,6 @@
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> - Shailaja Theme</p>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ Week1/Day2/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+    <h2>Welcome to my WordPress site!</h2>
+    <p>This is a simple custom theme developed as part of my QTech internship.</p>
+</main>
+
+<?php get_footer(); ?>
